711e5ee3d8443daae32e70e5ea73d14c39553459
Chinmay Aggarwal <chinmayagg27308@gmail.com>
2025-09-23 17:36:08 +0530

    Initial setup for WMS Common Library with Spring Boot 3.5.6, Kotlin - Shared utilities and JWT components

---
+++ src/main/kotlin/com/wmspro/common/WmsCommonApplication.kt
@@ -0,0 +1,11 @@
+package com.wmspro.common
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WmsCommonApplication
+
+fun main(args: Array<String>) {
+	runApplication<WmsCommonApplication>(*args)
+}
